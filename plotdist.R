02b8356364318817ddcde3ea18e534463ac95dce
sickel <sickel@61541d2e-7024-de11-9902-001c23e12ab7>
2012-10-17 06:12:36 +0000

    databasetilkobling og foreløpig plott
    git-svn-id: svn+ssh://sickel.net/home/users/sickel/svn/hanne_dataanalyse/trunk@65 61541d2e-7024-de11-9902-001c23e12ab7

--- plotdist.R
+++ plotdist.R
@@ -1,4 +1,15 @@
 library(DBI)
 library(RPostgreSQL)
 drv=dbDriver("PostgreSQL")
-con=dbConnect(drv,"morten","hobidor","localhost","beitedata")
+con=dbConnect(drv,"postgres","postgres","localhost","beitedata")
+
+
+cowid=286
+date='2008-08-11'
+
+sql=paste('select * from movement where cowid=',cowid," and datetime::date='",date,"'",sep='')
+rs=dbSendQuery(con,statement=sql)
+data=fetch(rs,n=-1)
+
+
+ plot(data$datetime,data$meters_obs,type='l')
