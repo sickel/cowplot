b435dfea8894d72a966e92b99d108ed7bcb55ecf
sickel <sickel@61541d2e-7024-de11-9902-001c23e12ab7>
2012-10-18 08:17:26 +0000

    Bruker nytt view. Beregner distanser i R
    git-svn-id: svn+ssh://sickel.net/home/users/sickel/svn/hanne_dataanalyse/trunk@66 61541d2e-7024-de11-9902-001c23e12ab7

--- plotdist.R
+++ plotdist.R
@@ -7,9 +7,16 @@ con=dbConnect(drv,"postgres","postgres","localhost","beitedata")
 cowid=286
 date='2008-08-11'
 
-sql=paste('select * from movement where cowid=',cowid," and datetime::date='",date,"'",sep='')
+sql=paste('select * from gps_coord where cowid=',cowid," and date='",date,"'",sep='')
 rs=dbSendQuery(con,statement=sql)
 data=fetch(rs,n=-1)
 
 
- plot(data$datetime,data$meters_obs,type='l')
+# plot(data$datetime,data$meters_obs,type='l')
+plot(data$x,data$y)
+
+dists=c(NA)
+for (i in (2:length(data$x))){
+    dist[i]=sqrt((data$x[i]-data$x[i-1])^2+(data$y[i]-data$y[i-1])^2)
+}
+    
