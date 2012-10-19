6fbf1aef0f5581d7691fafca6349b32e5df5faed
sickel <sickel@61541d2e-7024-de11-9902-001c23e12ab7>
2012-10-19 06:12:58 +0000

    Analyse av tilbakelagt distanse og forflytning over definert tid
    git-svn-id: svn+ssh://sickel.net/home/users/sickel/svn/hanne_dataanalyse/trunk@68 61541d2e-7024-de11-9902-001c23e12ab7

---
+++ distancedriver.R
@@ -0,0 +1,17 @@
+
+library(DBI)
+library(RPostgreSQL)
+drv=dbDriver("PostgreSQL")
+con=dbConnect(drv,"postgres","postgres","localhost","beitedata")
+
+
+cowid=286
+date='2008-08-11'
+
+data=fetchdata(cowid,date)
+# plot(data$datetime,data$meters_obs,type='l')
+plot(data$x,data$y)
+
+delta=12*5
+distplot(data,delta)
+
--- plotdist.R
+++ plotdist.R
@@ -1,22 +1,49 @@
-library(DBI)
-library(RPostgreSQL)
-drv=dbDriver("PostgreSQL")
-con=dbConnect(drv,"postgres","postgres","localhost","beitedata")
 
 
-cowid=286
-date='2008-08-11'
-
-sql=paste('select * from gps_coord where cowid=',cowid," and date='",date,"'",sep='')
-rs=dbSendQuery(con,statement=sql)
-data=fetch(rs,n=-1)
-
-
-# plot(data$datetime,data$meters_obs,type='l')
-plot(data$x,data$y)
-
-dists=c(NA)
-for (i in (2:length(data$x))){
-    dist[i]=sqrt((data$x[i]-data$x[i-1])^2+(data$y[i]-data$y[i-1])^2)
+distance=function(data,delta){
+  dists=c()
+  l=length(data$x)
+  for (i in (delta+1:l)){
+    dists[i]=sqrt((data$x[i]-data$x[i-delta])^2+(data$y[i]-data$y[i-delta])^2)
+  }
+  dists=dists[1:l]
+  return(dists)
 }
-    
+
+travel=function(dists,delta){
+  l=length(dists)
+  trav=c()  
+  for (i in (delta+1:l)){
+      trav[i]=sum(dists[(i-delta):i])
+  }
+  trav=trav[1:l]
+  return(trav)
+}
+
+
+distplot=function(data,delta){
+  dists5s=distance(data,1)
+  # plot(data$datetime,dists5s,type='l',ylim=c(0,100))
+  dists=distance(data,delta)
+  plot(data$datetime,dists,col="1",type='l')
+  trav=travel(dists5s,delta)
+  lines(data$datetime,trav,col="2")
+  /*
+	Burde satt navn som dists<delta> og trav<delta> - dermed er det mulig å
+	kjøre flere ganger med samme datasett og forskjellige delta-verdier
+  */
+  data$dists=dists
+  data$trav=trav
+  invisible(data)
+}
+
+fectchdata=function(cowid,date){
+  sql=paste('select * from gps_coord where cowid=',cowid," and date='",date,"'",sep='')
+  rs=dbSendQuery(con,statement=sql)
+  data=fetch(rs,n=-1)
+  return(data)	
+}
+
+
+
+
