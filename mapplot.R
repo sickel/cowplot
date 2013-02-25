6af8c4d4bc88c4280286027a0e7bb7e556bd066e
Morten Sickel <Morten@Sickel.net>
2013-02-25 17:06:25 +0100

    funksjoner for å plotte kart for alle dagene, diverse layoutoppydding

--- mapplot.R
+++ mapplot.R
@@ -1,12 +1,11 @@
+#
+# Plots a base map with legend
+#
+lgncex=0.3 # defines size of the legend
+
 plotmap=function(){
   if(!(exists('lok'))) lok='Valdres'
-  lgncex=0.22 # defines size of the legend
   if(!(exists('map'))) map=fetchmap(lok)
-  if(!(exists('od'))) {
-    od=fetchgpsobslok(lok)
-    od$obstype=as.factor(od$obstype)
-    od=od[order(od$obstype),]
-  }
   if(lok=='Valdres'){
     legy=bbox(map)[2,1]+1500
     legx=bbox(map)[1,1]-500
@@ -15,18 +14,16 @@ plotmap=function(){
   }else{
     legy=bbox(map)[2,2]+500
     legx=bbox(map)[1,1]-450
-    xlim=c(bbox(map)[1,1]-300,bbox(map)[1,2]+50)
+    xlim=c(bbox(map)[1,1]-300,bbox(map)[1,2]+50) # Need some extra space for legend
     palette(c("LightGrey","Black","LawnGreen","cornflower blue","RosyBrown","SandyBrown","DarkKhaki","DarkSeaGreen","PaleGreen","LightSeaGreen","DarkGoldenrod","YellowGreen","MediumSeaGreen","SpringGreen","wheat","peru","PaleGreen3","Yellow3","AntiqueWhite","LemonChiffon","thistle","ForestGreen","LightPink","LightPink","OliveDrab","DarkOliveGreen","Green","Gold","SaddleBrown","LightGray","coral","DarkOrange3","DarkOrchid4","IndianRed","firebrick","LemonChiffon","khaki","LightGray","Black","SpringGreen","Green Yellow"))
   }
-
-
-  lwd=par("lwd")
-  par(lwd=0.2)
-  plot(map,col=map$categorycode,xlim=xlim)
+  plot(map,col=map$categorycode,xlim=xlim,lwd=0.2)
   types=unique(map$category)
   nums=unique(map$categorycode)
   legend(legx,legy,types,fill=nums,border=nums,cex=lgncex,bty="n")
-  if(length(od[,1]>0)){
+}
+
+obspointplot=function(od){
     points(od$x,od$y,col='white',pch=19,cex=0.6)
     palette(c("red","blue","green","yellow","white"))
     points(od$x,od$y,col=as.integer(od$obstype),pch=19,cex=0.45)
@@ -38,9 +35,8 @@ plotmap=function(){
       legx=bbox(map)[1,1]+750
     }
     legend(legx,legy,levels(od$obstype),col=palette(),cex=lgncex,pch=19,bty="n")
-  }
-  par(lwd=lwd)
 }
+
 
 fetchodday=function(date){
   od=fetchobs(NULL,date)
@@ -49,22 +45,55 @@ fetchodday=function(date){
 }
 
 dayplot=function(date){
+  lwd=1
   logs=logdays(date=date)
   herd=logs$cowid
+  palette(c("cyan","magenta","orange","yellow","purple"))
   for(i in c(1:length(herd))){
     cow=herd[i]
     data=fetchdata(cow,date)
-    lines(data$x,data$y,col=i)
+    if(length(data)>0){
+      lines(data$x,data$y,col=i,lwd=lwd)
+      text(data$x,data$y,labels=data$marker-1,cex=0.3)
+    }
   }
   if(lok=="Valdres"){
     legy=bbox(map)[2,1]+500
     legx=bbox(map)[1,1]-300
   }else{
     legy=bbox(map)[2,2]+500
-    legx=bbox(map)[1,1]+750
+    legx=bbox(map)[1,1]+950
   }
-  legend(legx,legy,herd,col=c(1:length(herd)),cex=lgncex,lwd=1,bty="n")
+  legend(legx,legy,herd,col=c(1:length(herd)),cex=lgncex,lwd=lwd,bty="n")
 }
 
   
-  
+if(FALSE){
+   if(!(exists('od'))) {
+    od=fetchgpsobslok(lok)
+    od$obstype=as.factor(od$obstype)
+    od=od[order(od$obstype),]
+  }
+}
+
+
+plotdatetrack=function(date){
+  if(pdfplot) pdf(paste(lok,"_track_",date,".pdf",sep=""),paper="a4",width=0,height=0)
+  else
+  plotmap()
+  od=fetchodday(date)
+  if(length(od)>0) obspointplot(od)
+  dayplot(date)
+  title(main=date)
+  if(pdfplot) dev.off()
+}
+
+
+plotalltracs=function(lok){
+map=fetchmap(lok)
+for(day in unique(days[days$lokalitet==lok,2])){
+  day=format(as.Date(day,origin="1970-01-01"))
+  print(day)
+ plotdatetrack(day)
+}
+}
