599aa94baa6237fa531d8eacf27a53f6eaed9c95
Morten Sickel <Morten@Sickel.net>
2013-08-29 16:04:34 +0200

    Tools for making svg maps

--- tooltip.js
+++ tooltip.js
@@ -75,14 +75,14 @@ function mark(evt){
     var svgNS = "http://www.w3.org/2000/svg";
     var target = evt.currentTarget;
     
-    if (!(markedpoly==null)){
-	markedpoly.style.fill=oldcol;
-    }
+//    if (!(markedpoly==null)){
+//	markedpoly.style.fill=oldcol;
+//   }
     var markpoly=target.firstChild;
     while(markpoly != null && markpoly.nodeType == 3){ // skip TextNodes
 	markpoly = markpoly.nextSibling;
     }
-    oldcol=markpoly.attributes.fill;
+//    oldcol=markpoly.attributes.fill;
     markpoly.style.fill = 'yellow';
     markedpoly=markpoly;
 }
