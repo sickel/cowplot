6e31c8309916b350f29245e1ce2abf2e063b9e08
Morten Sickel <morten@sickel.net>
2015-01-27 19:17:07 +0100

    Updated to use rpart and randomforest

---
+++ gghistograms.R
@@ -0,0 +1,107 @@
+#"source('gpslib.R')
+#source('distancedriver.R')
+#lok=Sys.getenv('RHISTLOK')
+if(lok=='') lok='Valdres' 
+lokname=ifelse(lok=='Geilo','Hallingdal',lok)
+if(lok=='Geilo'){
+  letters=c('b','d','f')
+}else{
+  letters=c('a','c','e')}
+sql=paste("select * from defaultmodelrun where lokalitet='",lok,"'",sep='')
+rs=dbSendQuery(con,statement=sql)
+modelparams=fetch(rs,n=-1)
+rspeed=modelparams$restspeed/60
+wspeed=modelparams$walkspeed/60
+rratio=modelparams$restratio
+wratio=modelparams$walkratio
+rtyp=modelparams$resttyp
+wtyp=modelparams$walktyp
+#inttime=as.numeric(Sys.getenv('RINTTIME'))
+if(is.na(inttime)) inttime=5
+cat("============\n")
+cat(lok,"\n")
+cat(inttime,"\n")
+cat("============\n")
+par=par()
+size=1	
+nbreak=30
+cex.lab=size
+cex.axis=size
+cex.main=size
+cex.sub=size
+
+par(mai=c(1.02,2.0,0.82,0.42))
+if(!(exists('obsdist'))) obsdist=observationdistances(inttime)
+lokobs=obsdist[obsdist$lokalitet==lok,]
+lokobs$distms=lokobs[,paste('dists',inttime,'min',sep='')]/(inttime*60)
+lokobs$travms=lokobs[,paste('trav',inttime,'min',sep='')]/(inttime*60)
+lokobs$ratio=lokobs[,paste('ratio',inttime,'min',sep='')]
+#quantile
+quants=c()
+for (b in c('walking','grazing','resting')){
+
+  file=paste(lokname,b,inttime,'movement.png',sep='_')
+  png(file)
+  assign(paste("hist_movement",inttime,b,sep="_"),hist(lokobs$travms[lokobs$obstype==b],xlab='m/s',main=paste(lokname,b,"- movement",inttime,"mins"),xlim=c(0,1.5),breaks=c(0:nbreak)/nbreak*1.5,cex.lab=size,cex.axis=size,cex.main=size,cex.sub=size,lwd=size,col='gray',mai=c(1,2,1,0.5)))
+# die()
+  dev.off()
+  print(file)
+  print(quantile(lokobs$travms[lokobs$obstype==b],c(0.05,0.95),na.rm=TRUE))
+
+
+
+
+  file=paste(lokname,b,inttime,'displacement.png',sep='_')
+  png(file)
+  assign(paste("hist_displacement",inttime,b,sep="_"),hist(lokobs$distms[lokobs$obstype==b],xlab='m/s',main=paste(lokname,b,"- displacement",inttime,"mins"),xlim=c(0,1.5),breaks=c(0:nbreak)/nbreak*1.5,cex.lab=size,cex.axis=size,cex.main=size,cex.sub=size,lwd=size,col='gray',mai=c(1,2,1,0.5)))
+  dev.off()
+  print(file)
+  print(quantile(lokobs$distms[lokobs$obstype==b],c(0.05,0.95),na.rm=TRUE))
+
+  file=paste(lokname,b,inttime,'ratio.png',sep='_')
+  png(file)
+  assign(paste("hist_ratio",inttime,b,sep="_"),hist(lokobs$ratio[lokobs$obstype==b],xlab='',main=paste(lokname,b,"- ratio",inttime,"mins"),xlim=c(0,1),breaks=c(0:nbreak)/nbreak,cex.lab=size,cex.axis=size,cex.main=size,cex.sub=size,lwd=size,col='gray',mai=c(1,2,1,0.5)))
+  dev.off()
+  print(file)
+  print(quantile(lokobs$ratio[lokobs$obstype==b],c(0.05,0.95),na.rm=TRUE))
+}
+
+w=500
+h=350
+size=1.5
+l=1
+for(var in c('displacement','movement','ratio')){
+  unit='m/s'
+  
+  if(var=='ratio'){
+	unit=''
+	div=30
+  }
+  file=paste(lokname,inttime,var,'.png',sep='_')
+  # png(file,width=w,height=h)
+  
+  grz=c(get(paste('hist',var,inttime,'grazing',sep='_'))$counts,0)
+  rst=c(get(paste('hist',var,inttime,'resting',sep='_'))$counts,0)
+  wlk=c(get(paste('hist',var,inttime,'walking',sep='_'))$counts,0)
+  brks=get(paste('hist',var,inttime,'walking',sep='_'))$breaks
+  df=data.frame(n=grz,brks,activity="grazing")
+  df=rbind(df,data.frame(n=rst,brks,activity="resting"))
+  df=rbind(df,data.frame(n=wlk,brks,activity="walking"))
+  plt=ggplot(df,aes(x=brks,y=n,color=activity,linetype=activity))+labs(title=paste(lokname,var))
+  plt=plt+xlab(unit)
+  if((var=='displacement' && rtyp=='d') || (var=='movement' && rtyp=='t'))
+    plt=plt+geom_vline(xintercept=rspeed)
+  if((var=='displacement' && wtyp=='d') || (var=='movement' && wtyp=='t'))
+    plt=plt+geom_vline(xintercept=wspeed)
+  if(var=='ratio'){
+    if(rratio>0) plt=plt+geom_vline(xintercept=rratio)
+    if(wratio<1) plt=plt+geom_vline(xintercept=wratio)
+  }   
+# mtext(paste(letters[l],")",sep=''),line=2,at=c(-0.15*max(brks)),cex=1.5)
+  l=l+1
+  plt=plt+geom_step()+theme_bw()
+  plt=plt+scale_colour_manual(values=c('black','#555555','#999999'))
+  print(plt)
+  ggsave(file)
+}
+
